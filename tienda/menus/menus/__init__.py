90265a43d0c8c3dedd47958a9a48219156e596f8
ignacio buchter <111471268+Naachooo@users.noreply.github.com>
2023-06-13 20:10:30 -0300

    Menús hechos en otra ubicación

--- tienda/menus/menus/__init__.py
+++
@@ -1,7 +0,0 @@
-from .menuremeras import MenuRemeras
-from .menubuzos import MenuBuzos
-from .menucamperas import MenuCamperas
-from .menujeans import MenuJeans
-from .menupantalones import MenuPantalones
-from .menucamisas import MenuCamisas
-from .menutrajes import MenuTrajes
